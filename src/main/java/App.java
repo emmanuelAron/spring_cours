7727b157f7d78366a0562407aff1ffb746c5cb46
ARON-SAMUEL <emmanuel.aron@gmail.com>
2025-05-16 12:06:51 +0200

    tp4 injection

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,13 @@
 import com.mycompany.dvdstore.controller.MovieController;
-import com.mycompany.dvdstore.repository.GoLiveMovieRepository;
-import com.mycompany.dvdstore.service.MovieService;
-import com.mycompany.dvdstore.service.MovieServiceInterface;
+import com.mycompany.dvdstore.repository.FileMovieRepository;
+import com.mycompany.dvdstore.service.DefaultMovieService;
 
 public class App {
     public static void main(String[] args) {
 
         MovieController movieController = new MovieController();
-        GoLiveMovieRepository movieRepository = new GoLiveMovieRepository();
-        MovieService movieService = new MovieService();
+        FileMovieRepository movieRepository = new FileMovieRepository();
+        DefaultMovieService movieService = new DefaultMovieService();
         movieService.setMovieRepository(movieRepository);
 
         movieController.setMovieService(movieService);
